eaa548621e3797a744a3b156198083842c16e155
Anastasiya Staravoitava <nastua.matveeva@gmail.com>
2021-12-03 16:12:42 +0100

    hw moodpanda done

--- src/test/java/MoodPandaTest.java
+++ src/test/java/MoodPandaTest.java
@@ -8,38 +8,33 @@ public class MoodPandaTest extends BaseTest {
     private static final String MOOD_BADGE = "1";
     private static final String REPLY_MESSAGE = "Hola";
 
-    @Test
-    @Description("Login and updating mood via Update Mood btn > verifying message about successful update")
+    @Test(description = "Login and updating mood via Update Mood btn > verifying message about successful update")
     public void loginAndVerifyMoodUpdatedTest() {
         myDiarySteps.loginAndUpdateMood("tms_qa08onl_3@mailinator.com", "123456", 1);
         Assert.assertEquals(moodUpdatedModalPage.getSuccessfullyUpdatedMoodText(), EXPECTED_UPDATED_MOOD_TEXT);
     }
 
-    @Test
-    @Description("verifying updated mood badge with the last one displayed on My Diary page")
+    @Test(description = "verifying updated mood badge with the last one displayed on My Diary page")
     public void verifyMoodUpdatedBadgeTest() {
         myDiarySteps.loginAndUpdateMood("tms_qa08onl_3@mailinator.com", "123456", 1);
         Assert.assertEquals(myDiaryPage.getBadgeMoodNumber(), MOOD_BADGE);
     }
 
-    @Test
-    @Description("entering reply message and verifying that expected message is displayed")
+    @Test(description = "entering reply message and verifying that expected message is displayed")
     public void verifyReplyTest() {
         myDiarySteps.loginAndUpdateMood("tms_qa08onl_3@mailinator.com", "123456", 1);
         myDiaryPage.enterReplyIntoField(REPLY_MESSAGE);
         Assert.assertEquals(myDiaryPage.getReplyMessage(), REPLY_MESSAGE);
     }
 
-    @Test
-    @Description("sending a hug to a user and verifying that it is sent")
+    @Test(description = "sending a hug to a user and verifying that it is sent")
     public void sendHugAndVerifyTest() {
         myDiarySteps.loginAndUpdateMood("tms_qa08onl_3@mailinator.com", "123456", 1);
         myDiaryPage.clickSendHugBtn();
         Assert.assertTrue(myDiaryPage.isHugSendMessageDisplayed());
     }
 
-    @Test
-    @Description("Selecting a date from calender and verifying it on Diary page")
+    @Test(description = "Selecting a date from calender and verifying it on Diary page")
     public void setDateFromCalenderAndVerifyTest() {
         rateYourHappinessSteps.loginAndUpdateMood("tms_qa08onl_3@mailinator.com", "123456", 1);
         rateYourHappinessModalPage.selectDateFromCalender("Nov", "2021", "26")
@@ -48,8 +43,7 @@ public class MoodPandaTest extends BaseTest {
         Assert.assertEquals(myDiaryPage.getDateText(), "4 days ago");
     }
 
-    @Test
-    @Description("Logging in and out")
+    @Test(description = "Logging in and out")
     public void logOut() {
         loginPage.openPage()
                 .login("tms_qa08onl_3@mailinator.com", "123456")
